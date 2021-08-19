b479d84bd0a733056baf057f927903cdf7f636d6
Mario Antunes <mariolpantunes@gmail.com>
2021-08-19 12:49:53 +0100

    Initial code dump

---
+++ optimization/de.py
@@ -0,0 +1,88 @@
+# coding: utf-8
+
+__author__ = 'Mário Antunes'
+__version__ = '0.1'
+__email__ = 'mariolpantunes@gmail.com'
+__status__ = 'Development'
+
+
+import math
+import typing
+import joblib
+import logging
+import tempfile
+import numpy as np
+
+
+logger = logging.getLogger(__name__)
+
+
+# Cache from joblib
+location = tempfile.gettempdir()
+memory = joblib.Memory(location, verbose=0)
+
+
+# define mutation operation
+def mutation(x, F):
+    return x[0] + F * (x[1] - x[2])
+ 
+ 
+# define boundary check operation
+def check_bounds(mutated, bounds):
+    mutated_bound = [np.clip(mutated[i], bounds[i, 0], bounds[i, 1]) for i in range(len(bounds))]
+    return mutated_bound
+ 
+ 
+# define crossover operation
+def crossover(mutated, target, dims, cr):
+    # generate a uniform random value for every dimension
+    p = np.random.rand(dims)
+    # generate trial vector by binomial crossover
+    trial = [mutated[i] if p[i] < cr else target[i] for i in range(dims)]
+    return trial
+
+ 
+def differential_evolution(objective:typing.Callable, bounds:np.ndarray, n_iter:int=200, n_pop:int=20, F=0.5, cr=0.7):
+    # cache the initial objective function
+    objective_cache = memory.cache(objective)
+    
+    # initialise population of candidate solutions randomly within the specified bounds
+    pop = bounds[:, 0] + (np.random.rand(n_pop, len(bounds)) * (bounds[:, 1] - bounds[:, 0]))
+    # evaluate initial population of candidate solutions
+    obj_all = [objective_cache(ind) for ind in pop]
+    # find the best performing vector of initial population
+    best_vector = pop[np.argmin(obj_all)]
+    best_obj = min(obj_all)
+    prev_obj = best_obj
+    # run iterations of the algorithm
+    for i in range(n_iter):
+        # iterate over all candidate solutions
+        for j in range(n_pop):
+            # choose three candidates, a, b and c, that are not the current one
+            candidates = [candidate for candidate in range(n_pop) if candidate != j]
+            a, b, c = pop[np.random.choice(candidates, 3, replace=False)]
+            # perform mutation
+            mutated = mutation([a, b, c], F)
+            # check that lower and upper bounds are retained after mutation
+            mutated = check_bounds(mutated, bounds)
+            # perform crossover
+            trial = crossover(mutated, pop[j], len(bounds), cr)
+            # compute objective function value for target vector
+            obj_target = objective_cache(pop[j])
+            # compute objective function value for trial vector
+            obj_trial = objective_cache(trial)
+            # perform selection
+            if obj_trial < obj_target:
+                # replace the target vector with the trial vector
+                pop[j] = trial
+                # store the new objective function value
+                obj_all[j] = obj_trial
+        # find the best performing vector at each iteration
+        best_obj = min(obj_all)
+        # store the lowest objective function value
+        if best_obj < prev_obj:
+            best_vector = pop[np.argmin(obj_all)]
+            prev_obj = best_obj
+            # report progress at each iteration
+            logger.info('Iteration: %d f([%s]) = %.5f' % (i, np.around(best_vector, decimals=5), best_obj))
+    return [best_vector, best_obj]
